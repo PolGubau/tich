bbdb7f579e79da3b7f4223566ac38ebceb536bfa
Pol Gubau Amores <gubaupol@gmail.com>
2025-06-20 20:58:57 +0200

    first commit

---
+++ src/shared/components/Collapsible.tsx
@@ -0,0 +1,44 @@
+import { PropsWithChildren, useState } from 'react';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+ 
+import { Colors } from '@/src/shared/constants/Colors';
+import { ThemedText } from './ThemedText';
+import { useColorScheme } from '../hooks/useColorScheme.web';
+import { IconSymbol } from './ui/IconSymbol';
+
+export function Collapsible({ children, title }: PropsWithChildren & { title: string }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const theme = useColorScheme() ?? 'light';
+
+  return (
+    <View>
+      <TouchableOpacity
+        style={styles.heading}
+        onPress={() => setIsOpen((value) => !value)}
+        activeOpacity={0.8}>
+        <IconSymbol
+          name="chevron.right"
+          size={18}
+          weight="medium"
+          color={theme === 'light' ? Colors.light.icon : Colors.dark.icon}
+          style={{ transform: [{ rotate: isOpen ? '90deg' : '0deg' }] }}
+        />
+
+        <ThemedText type="defaultSemiBold">{title}</ThemedText>
+      </TouchableOpacity>
+      {isOpen && <View style={styles.content}>{children}</View>}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  heading: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 6,
+  },
+  content: {
+    marginTop: 6,
+    marginLeft: 24,
+  },
+});
